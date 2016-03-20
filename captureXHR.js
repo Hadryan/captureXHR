aad325cd21e66ac99aa0f35cf7c3b552be57b744
jackmasa <masa.sec@gmail.com>
2016-03-20 13:00:02 +0800

    Add Interceptor

--- captureXHR.js
+++ captureXHR.js
@@ -1,4 +1,5 @@
-function captureXHR(receive_url){
+function captureXHR(receive_url,interceptor){
+  if(typeof interceptor == 'undefined') interceptor ={};
   XMLHttpRequest.prototype._send = XMLHttpRequest.prototype.send;
   XMLHttpRequest.prototype._open = XMLHttpRequest.prototype.open;
   XMLHttpRequest.prototype.open = function(method, url, async, user, password){
@@ -6,10 +7,12 @@ function captureXHR(receive_url){
     a.href = url;
     url = escape(a.href);
     this.sucks = {method,url,async,user,password};
+    interceptor.onopen && (arguments = interceptor.onopen.apply(this,arguments)||arguments);
     this._open.apply(this,arguments);
   };
   XMLHttpRequest.prototype.send = function(data){
     this.sucks.body = data;
+    interceptor.onsend && (data = interceptor.onsend.call(this,data)||data);
     this._send.call(this,data);
     receive_url&&
       fetch(
